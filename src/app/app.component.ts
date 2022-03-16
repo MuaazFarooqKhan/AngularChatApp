3febb45872790f708da0d3bcd373f9930dd78ff4
MuaazFarooqKhan <kmuaaz18@gmail.com>
2022-03-17 01:03:29 +0500

    Integrating bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   conversation: any;
 
+  constructor(){}
   onConversationSelected(conversation: object) {
     this.conversation = conversation;
   }
